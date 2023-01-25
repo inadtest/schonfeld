66d6c36ff812e312ff6e451fa79a2101cc809d92
inadtest <inad45@gmail.com>
2023-01-25 00:43:36 -0500

    add readme

--- src/main/java/org/schonfeld/Application.java
+++ src/main/java/org/schonfeld/Application.java
@@ -3,6 +3,5 @@ package org.schonfeld;
 public class Application {
     public static void main(String[] args) {
         BetaCalculationEngine engine = new BetaCalculationEngine(args[0]);
-       // engine.calculateBeta()
     }
 }
--- src/test/java/org/schonfeld/BetaCalculationEngineTest.java
+++ src/test/java/org/schonfeld/BetaCalculationEngineTest.java
@@ -2,27 +2,21 @@ package org.schonfeld;
 
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BetaCalculationEngineTest {
-    private final BetaCalculationEngine underTest = new BetaCalculationEngine("src/test/resources/Test2.csv");
-    DateTimeFormatter formatter = new DateTimeFormatterBuilder()
-            .appendOptional(DateTimeFormatter.ofPattern("M/d/yyyy"))
-            .toFormatter();
 
     @Test
-    void shouldCalculateBetaTest() throws Exception {
+    void shouldCalculateBetaTest() {
         //given
-        LocalDate date = LocalDate.parse("9/28/2016", formatter);
+        BetaCalculationEngine underTest = new BetaCalculationEngine("src/test/resources/Test2.csv");
+        String startDate = "9/23/2016";
+        String endDate = "9/28/2016";
 
         //when
 
         //then
-        Double[] vals = underTest.calculateBeta("F", "SPY", "9/23/2016", "9/27/2016", 3L);
+        Double[] vals = underTest.calculateBeta("F", "SPY", startDate, endDate, 3L);
         assertEquals(vals[0], 0.0497);
     }
 }
